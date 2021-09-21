a11e15a71b9bb854212b8d82f1f8415db0bef116
Anton <normananton03@gmail.com>
2021-09-21 09:39:22 +0200

    token test i php

--- token-test.py
+++ token-test.py
@@ -1,7 +1,7 @@
 import secrets
-import uuid
+from uuid import uuid4
 
-token = uuid.uuid4()
+token = uuid4()
 token1 = secrets.token_bytes(16)
 
 print(token)
---
+++ token.php
@@ -0,0 +1,4 @@
+<?php
+$token = bin2hex(random_bytes(32));
+
+echo $token;
