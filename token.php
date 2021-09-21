38f94b193fca73c96af4c287f7ffda42e80a5d1b
Anton <normananton03@gmail.com>
2021-09-21 11:55:43 +0200

    Uppdaterad token.php

--- token.php
+++ token.php
@@ -1,4 +1,20 @@
 <?php
-$token = bin2hex(random_bytes(32));
+$token = bin2hex(random_bytes(24));
+?>
 
-echo $token;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Token Test</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo $token
+        ?>
+    </h1>
+</body>
+</html>
