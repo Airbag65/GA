276a2bfdf5a501aaa53f26eb984ea5781a1de3dc
Anton <normananton03@gmail.com>
2021-10-21 15:02:46 +0200

    functions.php påbörjad

--- src/backend/Account.php
+++ src/backend/Account.php
@@ -1,6 +1,25 @@
 <?php
+require_once "functions.php";
+
 class Account
 {
     public string $account;
-    public array $selected_account; 
+    public array $selectedAccount; 
+
+    public function __construct(
+        string $account,)
+    {
+        $this->account = $account;
+        $this->selectedAccount = selectAccount($this->account);
+    }
+
+
+    public function login()
+    {
+        $firstName = $this->selectedAccount["first_name"];
+        if($this->selectedAccount["token"] == ""){
+            generateToken($this->account);
+            $this->selectedAccount = selectAccount($this->account);
+        }
+    }
 }
