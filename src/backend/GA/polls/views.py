b70ed89cc10e82c21b73cd3951d7be2c32c7f220
Anton <normananton03@gmail.com>
2022-01-10 17:49:13 +0100

    Databas tabeller skapade

--- src/backend/GA/polls/views.py
+++ src/backend/GA/polls/views.py
@@ -1,8 +1,9 @@
-from requests import template
 from django.shortcuts import render
-from django.http import HttpResponse, response, Http404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
 from .models import Question
-from django.template import context, loader
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
