2f6211641c9bb28ba73e455caf5449cdb42daef9
Anton <normananton03@gmail.com>
2022-01-11 11:45:40 +0100

    uppdateringar

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,34 @@
+<?php
+require_once "../../vendor/autoload.php";
+
+$filename = "/home/anton/Desktop/GA/src/database/database.db";
+
+$dns = "sqlite:$filename";
+
+$pdo = new PDO($dns);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->exec('PRAGMA foreign_keys = ON');
+
+
+$sql = <<<EOD
+insert into patients(firstName, lastName, personNr, age)
+values('Bengt', 'Wallgren', 20000000, 100);
+EOD;
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$sqlu = <<<EOD
+select *
+from patients;
+EOD;
+$stmt = $pdo->prepare($sqlu);
+$stmt->execute();
+$data = $stmt->fetchAll();
+
+var_dump($data)
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
