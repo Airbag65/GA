02ece1ed704e554c43e81b8bc6c741c1aa05152f
elvlin274 <elvlin274@edu.linkoping.se>
2022-01-31 10:19:57 +0100

    Nya filnamn

--- public/loginTest.php
+++ public/loginTest.php
@@ -24,7 +24,7 @@ $pdo->exec('PRAGMA foreign_keys = ON');
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="login.css">
     <title>Logga in</title>
 </head>
 <body>
