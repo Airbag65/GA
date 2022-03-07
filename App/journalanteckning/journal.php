49bb7c6419980d4de3c084b850e5164b6c831c95
antnor211 <antnor211@edu.linkoping.se>
2022-03-07 12:10:46 +0100

    journal twig forts.

--- App/journalanteckning/journal.php
+++ App/journalanteckning/journal.php
@@ -56,15 +56,24 @@ $data["patientLastName"] = $patientData->lastName
 <body>
 <?php
 
-echo <<<patient
+$data["patientInfo"] = <<<patient
 <p>$patientData->firstName, $patientData->lastName</p>
 <p>Personnummer: $patientData->personNr</p>
 <p>Ålder: $patientData->age</p>
 patient;
+/*
+echo <<<patient
+<p>$patientData->firstName, $patientData->lastName</p>
+<p>Personnummer: $patientData->personNr</p>
+<p>Ålder: $patientData->age</p>
+patient;*/
+$patientId = $patientData->patientId;
 if(strtolower($patientData->bloodGroup) !== "okänd"){
-    echo "<p>Blodgrupp: $patientData->bloodGroup</p><br>";
+    $data["bloodGroup"] = "<p>Blodgrupp: $patientData->bloodGroup</p><br>";
+    //echo "<p>Blodgrupp: $patientData->bloodGroup</p><br>";
 }else{
-    echo "<a href='/save-bloodgroup/".$patientData->patientId."'>Lägg till blodgrupp</a><br>";
+    $data["bloodGroup"] = "<a href='/save-bloodgroup/$patientId'>Lägg till blodgrupp</a><br>";
+    //echo "<a href='/save-bloodgroup/$patientId>Lägg till blodgrupp</a><br>";
 }
 $data["vitals"] = <<<patient
 <p>Senaste Mätningar:</p>
@@ -73,7 +82,7 @@ $data["vitals"] = <<<patient
 <p>Blodmättnad: $patientData->spO2</p>
 <p>Tidigare Diagnoser: </p><br>
 patient;
-
+/*
 echo <<<patient
 <p>Senaste Mätningar:</p>
 <p>Blodtryck: $patientData->bloodPreasure</p>
@@ -82,9 +91,11 @@ echo <<<patient
 <p>Tidigare Diagnoser: </p><br>
 patient;
 
+
 foreach ($formerDiagnoses as $formerDiagnosis) {
     echo "<p>$formerDiagnosis</p>";
 }
+*/
 echo "<br>";
 foreach ($meetings as $meeting){
     $data["journalNote"][] = <<<eod
@@ -100,6 +111,7 @@ foreach ($meetings as $meeting){
         <p>Läkarens Kommentar: $meeting->comment</p>
     </div>
     eod;
+    /*
     echo <<<eod
     <div class='journal-anteckning'>
         <h4>Journalanteckning</h4>
@@ -113,11 +125,14 @@ foreach ($meetings as $meeting){
         <p>Läkarens Kommentar: $meeting->comment</p>
     </div>
     eod;
+    */
 }
 ?>
+<!--
 <br>
 <a href="/">Tillbaka</a>
 </body>
 </html>
+-->
 <?php
 rendering("views", "journal.twig", $data);
