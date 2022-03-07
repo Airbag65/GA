e9f769c44da09dddb6e3eaa0e1fabf5a4c949962
antnor211 <antnor211@edu.linkoping.se>
2022-03-07 11:00:18 +0100

    start twig journal

--- App/journalanteckning/journal.php
+++ App/journalanteckning/journal.php
@@ -7,6 +7,8 @@ if(!isset($_SESSION['createAttempt'])){
     $_SESSION['createAttempt'] = false;
 }
 
+$data = [];
+
 $pdo = initDb();
 
 $personellId = intval($_SESSION['id']);
@@ -38,6 +40,9 @@ if(strtolower($patientData->diagnoses) === "inga diagnoser"){
 }else{
     $formerDiagnoses = explode(";", $patientData->diagnoses);
 }
+$data["formerDiagnoses"] = $formerDiagnoses;
+$data["patientFirstName"] = $patientData->firstName;
+$data["patientLastName"] = $patientData->lastName
 ?>
 <!doctype html>
 <html lang="en">
@@ -61,6 +66,14 @@ if(strtolower($patientData->bloodGroup) !== "okänd"){
 }else{
     echo "<a href='/save-bloodgroup/".$patientData->patientId."'>Lägg till blodgrupp</a><br>";
 }
+$data["vitals"] = <<<patient
+<p>Senaste Mätningar:</p>
+<p>Blodtryck: $patientData->bloodPreasure</p>
+<p>Puls: $patientData->pulse</p>
+<p>Blodmättnad: $patientData->spO2</p>
+<p>Tidigare Diagnoser: </p><br>
+patient;
+
 echo <<<patient
 <p>Senaste Mätningar:</p>
 <p>Blodtryck: $patientData->bloodPreasure</p>
@@ -68,11 +81,25 @@ echo <<<patient
 <p>Blodmättnad: $patientData->spO2</p>
 <p>Tidigare Diagnoser: </p><br>
 patient;
+
 foreach ($formerDiagnoses as $formerDiagnosis) {
     echo "<p>$formerDiagnosis</p>";
 }
 echo "<br>";
 foreach ($meetings as $meeting){
+    $data["journalNote"][] = <<<eod
+    <div class='journal-anteckning'>
+        <h4>Journalanteckning</h4>
+        <p>Personal: $meeting->lastName, $meeting->firstName</p>
+        <p>Datum för journalanteckning: $meeting->date</p><br>
+        <p>Vitala parametrar vid besök:</p>
+        <p>Blodtryck: $meeting->blodtryck</p>
+        <p>Puls: $meeting->puls</p>
+        <p>Mättnad: $meeting->mattnad</p><br>
+        <p>Diagnos: $meeting->diagnosis</p>
+        <p>Läkarens Kommentar: $meeting->comment</p>
+    </div>
+    eod;
     echo <<<eod
     <div class='journal-anteckning'>
         <h4>Journalanteckning</h4>
@@ -92,3 +119,5 @@ foreach ($meetings as $meeting){
 <a href="/">Tillbaka</a>
 </body>
 </html>
+<?php
+rendering("views", "journal.twig", $data);
